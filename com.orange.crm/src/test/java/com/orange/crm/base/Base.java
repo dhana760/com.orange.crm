03f0ce980b33ed46ec6c970d52a16a494cb2984b
dhana <dhana@Litty>
2026-05-07 21:15:45 +0530

    Updating POM

---
+++ com.orange.crm/src/test/java/com/orange/crm/base/Base.java
@@ -0,0 +1,31 @@
+package com.orange.crm.base;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Base {
+
+	WebDriver driver;
+	
+	@BeforeMethod
+	void setup() {
+		
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+	}
+	
+	@AfterMethod
+	void tearDown()
+	{
+		driver.quit();
+	}
+}
